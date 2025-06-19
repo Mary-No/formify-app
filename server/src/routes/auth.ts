49066251b04b5b823f09b8b80ba2afe49690829a
Mary-No <mascha.novik@gmail.com>
2025-06-19 23:03:05 +0200

    change frontend port

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -41,7 +41,7 @@ router.get('/auth/google', passport.authenticate('google', {
 router.get('/auth/google/callback', passport.authenticate('google', {
     failureRedirect: '/login',
 }), (req, res) => {
-    res.redirect('http://localhost:3000')
+    res.redirect('http://localhost:5173')
 })
 
 
