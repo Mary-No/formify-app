acdbb7bc32de932249f633c9e21a572c2203cb0f
Mary-No <mascha.novik@gmail.com>
2025-06-20 12:57:09 +0200

    change google path

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -34,11 +34,11 @@ router.post('/register', handleRequest(async (req, res) => {
         })
 }))
 
-router.get('/auth/google', passport.authenticate('google', {
+router.get('/google', passport.authenticate('google', {
     scope: ['profile', 'email'],
 }))
 
-router.get('/auth/google/callback', passport.authenticate('google', {
+router.get('/google/callback', passport.authenticate('google', {
     failureRedirect: '/login',
 }), (req, res) => {
     res.redirect('http://localhost:5173')
