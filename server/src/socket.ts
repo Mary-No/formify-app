610a9465e46d4049ceaf93de795ba9fdec3afc7c
Mary-No <mascha.novik@gmail.com>
2025-06-26 13:36:24 +0200

    change socket.ts

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,21 +1,30 @@
 import { Server } from 'socket.io'
+import type { Server as HTTPServer } from 'http'
 
 let io: Server
 
-export const initSocket = (server: any) => {
+export const initSocket = (server: HTTPServer) => {
     io = new Server(server, {
         cors: {
-            origin: 'http://localhost:3000',
+            origin: [
+                'http://localhost:5173',
+                'https://mary-no.github.io',
+            ],
             credentials: true,
         },
     })
 
     io.on('connection', socket => {
         console.log('Socket connected:', socket.id)
+
         socket.on('join-template', (templateId: string) => {
             const room = `template-${templateId}`
             socket.join(room)
             console.log(`User ${socket.id} joined room ${room}`)
+        })
+
+        socket.on('disconnect', () => {
+            console.log('Socket disconnected:', socket.id)
         })
     })
 }
@@ -23,4 +32,9 @@ export const initSocket = (server: any) => {
 export const getIO = () => {
     if (!io) throw new Error('Socket.io not initialized')
     return io
+}
+
+export const emitNewComment = (templateId: string, comment: any) => {
+    const room = `template-${templateId}`
+    getIO().to(room).emit('new-comment', comment)
 }
