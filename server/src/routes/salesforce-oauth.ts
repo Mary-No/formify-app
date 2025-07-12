4eb5a116b0c43439e3f0afc96885d95abdb8634a
Mary-No <mascha.novik@gmail.com>
2025-07-12 19:42:55 +0200

    feat: change redirect url

--- server/src/routes/salesforce-oauth.ts
+++ server/src/routes/salesforce-oauth.ts
@@ -33,7 +33,8 @@ router.get("/callback", async (req, res) => {
             refresh_token: response.data.refresh_token,
             expires_at: Date.now() + response.data.expires_in * 1000
         };
-        return res.redirect(`${process.env.CLIENT_URL!}/account`);
+        // return res.redirect(`${process.env.CLIENT_URL!}/account`);
+        return res.redirect(`http://localhost:5173/account`);
     } catch (err: any) {
         console.error("❌ Salesforce token exchange error:", err.response?.data || err.message);
         res.status(500).send(`Salesforce OAuth error: ${JSON.stringify(err.response?.data || err.message)}`);
